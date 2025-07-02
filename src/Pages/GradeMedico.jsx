eeb7ada0e1dc97606e2bc73e461e919cf272d824
ThiagoKagoiki <thiagoyck@gmail.com>
2025-07-02 12:50:21 -0300

    add consulta na web, apenas falta fazer o menudrop

--- src/Pages/GradeMedico.jsx
+++ src/Pages/GradeMedico.jsx
@@ -1,16 +1,49 @@
 import React, { useState } from 'react';
 import './GradeMedico.css'
+import { adicionarConsulta } from '../services/api';
 
 export const GradeMedico = () => {
+
+  const [horario, setHorario] = useState('')
+  const [medico, setMedico] = useState('')
+  const [emailUser, setEmailUser] = useState('')
+
+  const handlesubmit = async(e) => {
+    e.preventDefault()
+    if(!horario || !medico || !emailUser){
+      alert("Preencha todos os campos do formulário para adicionar uma consulta")
+      return
+    }
+
+    const dados = {horario, medico, emailUser}
+    console.log("#######TIPO#######", dados)
+
+    try{
+      const resposta = await adicionarConsulta(dados)
+      console.log(resposta.data)
+      setHorario('')
+      setMedico('')
+      setEmailUser('')
+    }catch(error){
+      console.error("Erro ao registrar consulta: ", error.response ? error.response.data : error.message)
+      alert("Erro ao adicionar consulta")
+      console.log("erro: ", error)
+    }
+
+  }
+
   return (
     <div className="container">
       <div className="wrapper">
         <div className="card">
           <div className="card-header">
             <h1 className="card-title">Consultas Médicas</h1>
-            <button className="add-button">
-              Adicionar Consulta
-            </button>
+            <form onSubmit={handlesubmit} className='addForm'>
+              <input type="text" placeholder='horario' value={horario} onChange={e => setHorario(e.target.value)}/>
+              <input type="text" placeholder='médico' value={medico} onChange={e => setMedico(e.target.value)}/>
+              <input type="text" placeholder='email do paciente' value={emailUser} onChange={e => setEmailUser(e.target.value)}/>
+              <button className="add-button">Adicionar Consulta</button>
+            </form>
           </div>
 
           <div className="grid">
